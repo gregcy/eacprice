48a89abb14d6c84584537e624a92469dfff5502d
Greg Andreou <greg.andreou@gmail.com>
2023-06-22 18:33:12 +0300

    Initial Commit

---
+++ app/Http/Controllers/FuelPriceAdjustmentController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\FuelPriceAdjustment;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class FuelPriceAdjustmentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index():Response
+    {
+        return response('Hello World!');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(FuelPriceAdjustment $fuelPriceAdjustment)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(FuelPriceAdjustment $fuelPriceAdjustment)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, FuelPriceAdjustment $fuelPriceAdjustment)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(FuelPriceAdjustment $fuelPriceAdjustment)
+    {
+        //
+    }
+}
