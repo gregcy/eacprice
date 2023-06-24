3c51240cbee8c6f7196702fc031c5bb1604b3c47
Greg Andreou <greg.andreou@gmail.com>
2023-06-24 14:53:29 +0300

    Working on form part 2

--- resources/views/fuel-price-adjustment/index.blade.php
+++ resources/views/fuel-price-adjustment/index.blade.php
@@ -1,7 +1,12 @@
 <x-app-layout>
-    <div class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
+    <div class="max-w-3xl mx-auto p-4 sm:p-6 lg:p-8">
         <form method="POST" action="{{ route('adjustments.store') }}">
             @csrf
+            <label for="Start Date">{{ __('Start Date') }}</label>
+            <input type="month"
+                name="Start Date"
+                class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
+            />
             <textarea
                 name="message"
                 placeholder="{{ __('What\'s on your mind?') }}"
