2bf801afe79141be3a1ac1b7631bb951dc9d6f0d
Greg Andreou <greg.andreou@gmail.com>
2023-06-26 00:02:46 +0300

    Started work on edit form

--- resources/views/fuel-price-adjustment/edit.blade.php
+++ resources/views/fuel-price-adjustment/edit.blade.php
@@ -1,7 +1,8 @@
 <x-app-layout>
     <div class="max-w-3xl mx-auto p-4 sm:p-6 lg:p-8">
-        <form method="POST" action="{{ route('adjustments.store') }}">
+        <form method="POST" action="{{ route('adjustments.update', $adjustment) }}">
             @csrf
+            @method('patch')
             <label for="start_date">{{ __('Start Date') }}</label>
             <input type="date"
                 name="start_date"
--- routes/web.php
+++ routes/web.php
@@ -31,7 +31,7 @@ Route::middleware('auth')->group(function () {
 });
 
 Route::resource('adjustments', FuelPriceAdjustmentController::class)
-    ->only(['index', 'store'])
+    ->only(['index', 'store', 'edit', 'update'])
     ->middleware(['auth', 'verified']);
 
 require __DIR__.'/auth.php';
