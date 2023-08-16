88ed152dc4ca4e2e156a1085b320d8ba6b48899a
Greg Andreou <greg.andreou@gmail.com>
2023-08-16 23:50:00 +0300

    EC-5 Initial implementation for rate 02

--- app/Http/Controllers/GetCurrentRate.php
+++ app/Http/Controllers/GetCurrentRate.php
@@ -87,6 +87,68 @@ class GetCurrentRate extends Controller
             }
         }
         else if ($tariffCode == '02') {
+            $time_now = date('H');
+
+            $current_energy_charge = 0;
+            $current_network_charge = 0;
+            $current_ancilary_services = 0;
+
+            if ($time_now >= 9 && $time_now <= 23) {
+                $current_energy_charge = $tariff->energy_charge_normal;
+                $current_network_charge = $tariff->network_charge_normal;
+                $current_ancilary_services = $tariff->ancilary_services_normal;
+            }
+            else {
+                $current_energy_charge = $tariff->energy_charge_reduced;
+                $current_network_charge = $tariff->network_charge_reduced;
+                $current_ancilary_services = $tariff->ancilary_services_reduced;
+            }
+            if ($creditUnits == "0") {
+                $json = [
+                    'Measurement' => '€/kWh',
+                    'Cost Per Unit' => (float) number_format(
+                        ($adjustment->revised_fuel_adjustment_price +
+                        $current_energy_charge +
+                        $current_network_charge +
+                        $current_ancilary_services +
+                        $tariff->public_service_obligation)*1.19, 6
+                    ),
+                    'Breakdown' => [
+                        'Energy Charge' => (float) number_format($current_energy_charge, 6),
+                        'Network Charge' => (float) number_format($current_network_charge, 6),
+                        'Ancilary Services' => (float) number_format($current_ancilary_services, 6),
+                        'Public Service Obligation' => (float) number_format($tariff->public_service_obligation, 6),
+                        'Fuel Adjustment' => (float) number_format($adjustment->revised_fuel_adjustment_price, 6),
+                        'VAT' => (float) number_format(
+                            0.19 * ($adjustment->revised_fuel_adjustment_price +
+                            $current_energy_charge +
+                            $current_network_charge +
+                            $current_ancilary_services +
+                            $tariff->public_service_obligation), 6
+                        ),
+                    ],
+                ];
+            }
+            else {
+                $json = [
+                    'Measurement' => '€/kWh',
+                    'Cost Per Unit' => (float) number_format(
+                        ($current_energy_charge +
+                        $current_network_charge +
+                        $tariff->public_service_obligation)*1.19, 6
+                    ),
+                    'Breakdown' => [
+                        'Network Charge' => (float) number_format($tariff->network_charge_normal, 6),
+                        'Ancilary Services' => (float) number_format($tariff->ancilary_services_normal, 6),
+                        'Public Service Obligation' => (float) number_format($tariff->public_service_obligation, 6),
+                        'VAT' => (float) number_format(
+                            0.19*($current_energy_charge +
+                            $current_network_charge +
+                            $tariff->public_service_obligation), 6
+                        ),
+                    ]
+                ];
+            }
 
         }
         else if ($tariffCode == '08') {
