048ec651fafad603f62d020069c41e51adb919a3
Greg Andreou <greg.andreou@gmail.com>
2023-10-10 23:24:37 +0300

    Work on EACTrait

--- app/Http/Traits/EACTrait.php
+++ app/Http/Traits/EACTrait.php
@@ -5,7 +5,57 @@ use App\Models\Tariff;
 use App\Models\Adjustment;
 
 trait EACTrait {
-    public function calculateEACCost($tariffCode, $consmption, $creditAmount) {
 
+    public function calculateEACCost01($tariffCode, $billing, $consmption, $creditUnits) {
+        $dateNow = date('Y-m-d', time());
+        $adjustment = Adjustment::where('consumer_type', $billing)
+            ->where('start_date', '<=', $dateNow)
+            ->where('end_date', '>=', $dateNow)
+            ->first();
+
+        $tariff = Tariff::where('code', $tariffCode)
+            ->where('end_date', '=', null)
+            ->first();
+
+        if ($creditUnits == 0) {
+            $cost = [
+                'Energy Charge' => (float) number_format($tariff->energy_charge_normal, 6),
+                'Network Charge' => (float) number_format($tariff->network_charge_normal, 6),
+                'Ancilary Services' => (float) number_format($tariff->ancilary_services_normal, 6),
+                'Public Service Obligation' => (float) number_format($tariff->public_service_obligation, 6),
+                'Fuel Adjustment' => (float) number_format($adjustment->revised_fuel_adjustment_price, 6),
+                'VAT' => (float) number_format(
+                    0.19 * ($adjustment->revised_fuel_adjustment_price +
+                    $tariff->energy_charge_normal +
+                    $tariff->network_charge_normal +
+                    $tariff->ancilary_services_normal +
+                    $tariff->public_service_obligation), 6
+                ),
+                'Total' => (float) number_format(
+                    ($adjustment->revised_fuel_adjustment_price +
+                    $tariff->energy_charge_normal +
+                    $tariff->network_charge_normal +
+                    $tariff->ancilary_services_normal +
+                    $tariff->public_service_obligation) * 1.19, 6
+                ),
+            ];
+        } else {
+            $cost = [
+                'Network Charge' => (float) number_format($tariff->network_charge_normal, 6),
+                'Ancilary Services' => (float) number_format($tariff->ancilary_services_normal, 6),
+                'Public Service Obligation' => (float) number_format($tariff->public_service_obligation, 6),
+                'VAT' => (float) number_format(
+                    0.19 * ($tariff->network_charge_normal +
+                    $tariff->ancilary_services_normal +
+                    $tariff->public_service_obligation), 6
+                ),
+                'Total' => (float) number_format(
+                    ($tariff->network_charge_normal +
+                    $tariff->ancilary_services_normal +
+                    $tariff->public_service_obligation) * 1.19, 6
+                ),
+            ];
+        }
+        return $cost;
     }
 }
