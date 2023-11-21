863aa48da625017b975d2a827ec6142e6096179f
Greg Andreou <greg.andreou@gmail.com>
2023-11-21 09:20:29 +0200

    EC-16 Started fixing desktop styles

--- resources/views/calculator/calculator.blade.php
+++ resources/views/calculator/calculator.blade.php
@@ -1,9 +1,9 @@
-<form id="eac-calculator" class="p-6 mb-12" method="POST" action="{{ route('calculator.calculate',['lang' => app()->getLocale()]) }}">
+<form id="eac-calculator" class="p-2 mb-12" method="POST" action="{{ route('calculator.calculate',['lang' => app()->getLocale()]) }}">
     @csrf
     <fieldset id="tariff" class="py-4 w-100">
-        <label for="tariff-select" class="text-lg font-medium pr-4 w-full block sm:inline sm:w-auto">{{ __('Tariff') }}:</label>
+        <label for="tariff-select" class="text-lg font-medium pr-2 w-full block md:inline md:w-auto">{{ __('Tariff') }}:</label>
         <select id="tariff-select" name="tariff"
-            class="inline-block grow border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm w-full block sm:inline sm:w-auto">
+            class="inline-block grow border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-md w-full block md:inline md:w-auto">
             @php
                 $options = [
                     '01' => __('01 - Single Rate Domestic Use'),
@@ -17,9 +17,9 @@
                 <option value="{{ $value }}" @if($selectedValue == $value) selected @endif>{{ $label }}</option>
             @endforeach
         </select>
-        <label for="period" class="text-lg font-medium pr-4 w-full block sm:inline sm:w-auto">{{ __('Month') }}:</label>
+        <label for="period" class="text-lg font-medium pr-2 pl-2 mt-4 w-full block md:inline md:w-auto">{{ __('Month') }}:</label>
         <select id="period" name="period"
-            class="inline-block grow border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm w-full block sm:inline sm:w-auto">
+            class="inline-block grow border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-md w-full block md:inline md:w-auto">
             @php
                 $options = [
                     '2023-01' => __('January') . ' 2023',
@@ -44,27 +44,27 @@
     </fieldset>
     <fieldset id="tariff01" class="{{ (isset($values) && in_array($values['tariff'], ['01', '08'])) || !isset($values) ? 'block' : 'hidden' }}">
         <div class="w-100">
-            <label for="consumption" class="text-lg font-medium pr-20 w-full block sm:inline sm:w-auto">{{ __('Consumption') }} (kWh):</label>
+            <label for="consumption" class="text-lg font-medium pr-20 w-full block md:inline md:w-auto">{{ __('Consumption') }} (kWh):</label>
             <input id="consumption" type="number" name="consumption" step="1" min="0" placeholder="0" value="{{ old('consumption', $values['consumption'] ?? 0) }}"
-            class="inline-block grow border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm w-full block sm:inline sm:w-auto">
+            class="inline-block grow border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-md w-full block md:inline md:w-auto">
         </div>
         <div class="w-100 py-4">
-            <label for="credit-amount" class="text-lg font-medium pr-4 w-full block sm:inline sm:w-auto">{{ __('Returned Solar Power') }} (kWh):</label>
+            <label for="credit-amount" class="text-lg font-medium pr-4 w-full block md:inline md:w-auto">{{ __('Returned Solar Power') }} (kWh):</label>
             <input id="credit-amount" type="number" name="credit-amount" step="1" min="0" placeholder="0" value="{{ old('credit-amount', $values['credit-amount'] ?? 0) }}"
-            class="inline-block grow border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm w-full block sm:inline sm:w-auto">
+            class="inline-block grow border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-md w-full block md:inline md:w-auto">
         </div>
     </fieldset>
 
     <fieldset id="tariff02" class="{{ (isset($values) && $values['tariff'] === '02') ? 'block' : 'hidden' }}">
         <div class="w-100">
-            <label for="consumption-standard" class="text-lg font-medium pr-5 w-full block sm:inline sm:w-auto">{{ __('Consumption During Standard Period') }} 09:00-23:00 (kWh):</label>
+            <label for="consumption-standard" class="text-lg font-medium pr-5 w-full block md:inline md:w-auto">{{ __('Consumption During Standard Period') }} 09:00-23:00 (kWh):</label>
             <input id="consumption-standard" type="number" name="consumption-standard" step="1" min="0" placeholder="0" value="{{ old('consumption-standard', $values['consumption-standard'] ?? 0) }}"
-            class="inline-block grow border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm w-full block sm:inline sm:w-auto">
+            class="inline-block grow border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-md w-full block md:inline md:w-auto">
         </div>
         <div class="w-100 py-4">
-            <label for="consumption-economy" class="text-lg font-medium pr-4 w-full block sm:inline sm:w-auto">{{ __('Consumption During Economy Period') }} 23:00-09:00 (kWh):</label>
+            <label for="consumption-economy" class="text-lg font-medium pr-4 w-full block md:inline md:w-auto">{{ __('Consumption During Economy Period') }} 23:00-09:00 (kWh):</label>
             <input id="consumption-economy" type="number" name="consumption-economy" step="1" min="0" placeholder="0" value="{{ old('consumption-economy', $values['consumption-economy'] ?? 0) }}"
-            class="inline-block grow border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm w-full block sm:inline sm:w-auto">
+            class="inline-block grow border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-md w-full block md:inline md:w-auto">
         </div>
     </fieldset>
 
