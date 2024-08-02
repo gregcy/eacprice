945931fdcc8e9af9c6d83b18a07427303e0c89b4
Greg Andreou <greg.andreou@gmail.com>
2024-08-02 23:54:30 +0300

    Better Fix for access issues on Production

--- app/Models/User.php
+++ app/Models/User.php
@@ -78,6 +78,6 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessPanel(Panel $panel): bool
     {
-        return $this->is_admin;
+        return true;
     }
 }
